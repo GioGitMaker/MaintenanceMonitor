3735d933ecc21a2476c6f182d2b937836c33274e
Margit90 <wi19b095@technikum-wien.at>
2021-06-19 20:21:00 +0200

    Change StaticController
    +@Path

--- src/main/java/at/resources/StaticController.java
+++ src/main/java/at/resources/StaticController.java
@@ -1,10 +1,13 @@
 package at.resources;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.InputStream;
 
+
+@Path("")
 public class StaticController {
 
     //https://stackoverflow.com/questions/8642920/how-to-serve-static-content-with-jax-rs/8643120#8643120
@@ -12,7 +15,6 @@ public class StaticController {
     @Produces(MediaType.TEXT_HTML)
     public InputStream getMessage() throws Exception{
         InputStream resource =getClass().getClassLoader().getResourceAsStream("MaintenanceMode.html");
-        System.out.println(resource);
         return resource;
     }
 }
